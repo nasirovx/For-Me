c2f94456668aec526293e54e4813aed7e8b1dc15
bekastannasirov <5ekastannasirov@gmail.com>
2024-03-20 13:18:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <div class='container'>
+        <div class='content'>
+            <div class='image'><img src='https://img.freepik.com/premium-photo/cartoon-man-with-glasses-and-beard-in-a-blue-shirt-generative-ai_958165-24562.jpg'/></div>
+            <span>BEKASTAN</span>
+            <div class='btns'><div>Fullstack Developer</div><div>Digital Craftsman</div></div>
+            <button class='btn'><a href="#">Newsletter</a></button>
+            <button class='btn'><a href="#">Website</a></button>
+            <button class='btn'><a target="_blank" href="https://youtube.com/@tutsu.?si=VigTpzCsIfaskJNi">YouTube</a></button>
+            <button class='btn'><a target="_blank" href="https://github.com/nasirovx">GitHub</a></button>
+            <button class='btn'><a target="_blank" href="https://t.me/beka_stan">Telegram</a></button>
+            <button class='btn'><a target="_blank" href="https://www.instagram.com/5ekastan?igsh=MTN5NXJjanpjN2F4Mw==">instagram</a></button>
+            <button class='btn'><a href="#">Blog Posts</a></button>
+        </div>
+    </div>
+  )
+}
+
+export default App
