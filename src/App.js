b067ecf60b86dd4fe5944d0dedf8ca7ebf5fdeea
Bekastan <5ekastan@example.com>
2024-07-24 06:10:32 +0300

    Resolved conflicts

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ const App = () => {
             <div class='image'><img src='https://img.freepik.com/premium-photo/cartoon-man-with-glasses-and-beard-in-a-blue-shirt-generative-ai_958165-24562.jpg'/></div>
             <span className='logo'>BEKASTAN</span>
             <div class='btns'><div>Fullstack Developer</div><div>Digital Craftsman</div></div>
-            <a href="https://www.linkedin.com/in/bekastan-nasirov-a37258301/"><button class='btn'><span>LinkedIn</span></button></a>
-            <a href="https://my-sait-b7cc.vercel.app/"><button class='btn'><span>WebSite</span></button></a>
+            <a target="_blank" href="https://www.linkedin.com/in/bekastan-nasirov-a37258301/"><button class='btn'><span>LinkedIn</span></button></a>
+            <a target="_blank" href="https://my-sait-b7cc.vercel.app/"><button class='btn'><span>WebSite</span></button></a>
             <a target="_blank" href="https://youtube.com/@tutsu.?si=VigTpzCsIfaskJNi"><button class='btn'><span>YouTube</span></button></a>
             <a target="_blank" href="https://github.com/nasirovx"><button class='btn'><span>GitHub</span></button></a>
             <a target="_blank" href="https://t.me/beka_stan"><button class='btn'><span>Telegram</span></button></a>
             <a target="_blank" href="https://www.instagram.com/5ekastan?igsh=MTN5NXJjanpjN2F4Mw=="><button class='btn'><span>instagram</span></button></a>
-            <a href="#"><button class='btn'><span>Blog Posts</span></button></a>
+            <a target="_blank" href="https://blog-a-pp-ten.vercel.app/"><button class='btn'><span>Blog Posts</span></button></a>
         </div>
     </div>
   )
