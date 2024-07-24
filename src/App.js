0a83067ec92629564d2f8252d4d5cc238554b429
Bekastan <5ekastan@example.com>
2024-07-24 06:05:15 +0300

    For Me

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ const App = () => {
     <div class='container'>
         <div class='content'>
             <div class='image'><img src='https://img.freepik.com/premium-photo/cartoon-man-with-glasses-and-beard-in-a-blue-shirt-generative-ai_958165-24562.jpg'/></div>
-            <span>BEKASTAN</span>
+            <span className='logo'>BEKASTAN</span>
             <div class='btns'><div>Fullstack Developer</div><div>Digital Craftsman</div></div>
-            <button class='btn'><a href="#">Newsletter</a></button>
-            <button class='btn'><a href="https://my-sait-b7cc.vercel.app/">Website</a></button>
-            <button class='btn'><a target="_blank" href="https://youtube.com/@tutsu.?si=VigTpzCsIfaskJNi">YouTube</a></button>
-            <button class='btn'><a target="_blank" href="https://github.com/nasirovx">GitHub</a></button>
-            <button class='btn'><a target="_blank" href="https://t.me/beka_stan">Telegram</a></button>
-            <button class='btn'><a target="_blank" href="https://www.instagram.com/5ekastan?igsh=MTN5NXJjanpjN2F4Mw==">instagram</a></button>
-            <button class='btn'><a href="#">Blog Posts</a></button>
+            <a href="https://www.linkedin.com/in/bekastan-nasirov-a37258301/"><button class='btn'><span>LinkedIn</span></button></a>
+            <a href="https://my-sait-b7cc.vercel.app/"><button class='btn'><span>WebSite</span></button></a>
+            <a target="_blank" href="https://youtube.com/@tutsu.?si=VigTpzCsIfaskJNi"><button class='btn'><span>YouTube</span></button></a>
+            <a target="_blank" href="https://github.com/nasirovx"><button class='btn'><span>GitHub</span></button></a>
+            <a target="_blank" href="https://t.me/beka_stan"><button class='btn'><span>Telegram</span></button></a>
+            <a target="_blank" href="https://www.instagram.com/5ekastan?igsh=MTN5NXJjanpjN2F4Mw=="><button class='btn'><span>instagram</span></button></a>
+            <a href="#"><button class='btn'><span>Blog Posts</span></button></a>
         </div>
     </div>
   )
